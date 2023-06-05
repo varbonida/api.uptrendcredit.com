0956c1545bc21842e089b7cb3d7b9dff582b467e
vergel <vergel.arbonida@gmail.com>
2023-06-05 19:32:53 +0800

    plaid

---
+++ app/Models/ConnectedAccount.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ConnectedAccount extends Model
+{
+    use HasFactory;
+
+	protected $fillable = ['user_id', 'connected_institution_id', 'account_id', 'mask', 'name', 'official_name', 'subtype', 'type'];
+}
